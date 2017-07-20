296a274b2a19afad106a904e92d395e25893bd94
Jeff Browning <jeff@craftcoded.com>
2017-07-20 14:02:41 -0700

    version 0.2 commit

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+ /* Require for Webpack File Loader.
+ * File Loader is a way to compile your Sass with Webpack.
+ * This is simply a link to your Sass entry point.
+ * This is removed from your final JavaScript build.
+ *
+ */
+require( "../scss/main.scss" );
